3961a6acc2b8da5d17d31be3306534fa93b086b6
Lukman Usman <lukman10usman@gmail.com>
2026-09-23 00:41:32 +0100

    Feat: Initialize Express backend

---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Ogah's ENT API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
